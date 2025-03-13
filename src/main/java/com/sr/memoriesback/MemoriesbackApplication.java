5cb6a1b4fd02a11628568878eacb226f1ed65f9d
surimgit <ruriaim@gmail.com>
2025-03-13 12:27:55 +0900

    first

---
+++ src/main/java/com/sr/memoriesback/MemoriesbackApplication.java
@@ -0,0 +1,13 @@
+package com.sr.memoriesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemoriesbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemoriesbackApplication.class, args);
+	}
+
+}
